bfff06733311b86bd58c09d82eb5e407239799b4
ishika1727 <ishikadubey2000@gmail.com>
2020-07-09 04:02:23 +0530

    api

---
+++ js/api.js
@@ -0,0 +1,27 @@
+function diet() {
+    document.getElementById("form").addEventListener("click", function (e) { e.preventDefault() });
+    var cal = document.getElementById("cal").value;
+    var time = document.getElementById("timespan").value;
+    var diet = document.getElementById("diet").value;
+    var exclude = document.getElementById("exclude").value;
+    console.log(cal);
+    console.log(time);
+    console.log(diet);
+    console.log(exclude);
+    var url = "https://api.spoonacular.com/mealplanner/generate?apiKey=ef4c92c9ef5d45e489e4f995a94055f3&timeFrame=" + time + "&targetCalories=" + cal + "&diet=" + diet + "&exclude=" + exclude + "";
+    var settings = {
+        "url": url,
+        "method": "GET",
+        "timeout": 0,
+        "headers": {
+            "Cookie": "__cfduid=dd11996d7deb2c7b62afad192c91560ff1594244494"
+        },
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+}
+
+
+
